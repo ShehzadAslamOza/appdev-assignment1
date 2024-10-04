ce4aa520d7177a94eae2c1517292090922be687e
Shehzad Aslam Oza <32712842+ShehzadAslamOza@users.noreply.github.com>
2024-10-04 14:27:47 +0500

    Added App Bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(const MainApp());
+  runApp(const JobListingPage());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class JobListingPage extends StatelessWidget {
+  const JobListingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello!'),
+        appBar: AppBar(
+          title: const Row(
+            mainAxisAlignment:
+                MainAxisAlignment.start, // Align text to the left
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 6),
+                child: Text(
+                  'Jobs',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 16.0),
+              child: Icon(Icons.notifications_none), // Static notification icon
+            ),
+          ],
         ),
+        body: Center(
+          child: Text('Job Listing 123!'),
+        ),
+        bottomNavigationBar: Text("Bottom Navigation Bar"),
       ),
     );
   }
