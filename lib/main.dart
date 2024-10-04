e7f7864d7dc03a369cb95ebfc1974de732654c8e
ShehzadAslamOza <shehzadaslamoza@gmail.com>
2024-10-04 16:40:27 +0500

    Added Navigaion Bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,104 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(const JobListingPage());
+  runApp(const JobListingApp());
 }
 
-class JobListingPage extends StatelessWidget {
-  const JobListingPage({super.key});
+class JobListingApp extends StatelessWidget {
+  const JobListingApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Row(
-            mainAxisAlignment:
-                MainAxisAlignment.start, // Align text to the left
-            children: [
-              Padding(
-                padding: EdgeInsets.only(left: 6),
-                child: Text(
-                  'Jobs',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-            ],
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: JobListingPage(),
+    );
+  }
+}
+
+class JobListingPage extends StatefulWidget {
+  const JobListingPage({super.key});
+
+  @override
+  State<JobListingPage> createState() => _JobListingPageState();
+}
+
+class _JobListingPageState extends State<JobListingPage> {
+  int currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+
+    
+    final List<Widget> pages = [
+      Center(
+        child: Text(
+          'Jobs Page',
+          style: theme.textTheme.titleLarge,
+        ),
+      ),
+      Center(
+        child: Text(
+          'Resume Page',
+          style: theme.textTheme.titleLarge,
+        ),
+      ),
+      Center(
+        child: Text(
+          'Settings Page',
+          style: theme.textTheme.titleLarge,
+        ),
+      ),
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Jobs',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
           ),
-          actions: [
-            Padding(
-              padding: const EdgeInsets.only(right: 16.0),
-              child: Icon(Icons.notifications_none), // Static notification icon
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(right: 16.0),
+            child: Icon(
+              Icons.notifications_none,
+              color: Colors.black,
             ),
-          ],
-        ),
-        body: Center(
-          child: Text('Job Listing 123!'),
-        ),
-        bottomNavigationBar: Text("Bottom Navigation Bar"),
+          ),
+        ],
+      ),
+      body: pages[currentPageIndex], 
+
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        indicatorColor: Colors.grey,
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Jobs',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.notifications_sharp),
+            label: 'Resume',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
       ),
     );
   }
